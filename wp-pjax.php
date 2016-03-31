ddbc362042069beaf3409ff799c6aaa7da754952
Nerijus Eimanavičius <nerijus@eimanavicius.lt>
2016-03-31 23:39:29 +0300

    Fixed comments and doc blocks in wp-pjax.php

--- wp-pjax.php
+++ wp-pjax.php
@@ -1,30 +1,33 @@
 <?php
 /**
  * Plugin Name: WP-PJAX
- * Plugin URI: http://wordpress.org/extend/plugins/wp-pjax/
+ * Plugin URI:  http://wordpress.org/extend/plugins/wp-pjax/
  * Description: Makes Wordpress use the PJAX (PushState + AJAX) technique for loading content
- * Version: 0.0.4.1.1
- * Author: Peter Elmered
- * Author URI: http://elmered.com
+ * Version:     0.0.4.1.1
+ * Author:      Peter Elmered
+ * Author URI:  http://elmered.com
+ * License:     GPL2+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: pe_wp_pjax
- * License: http://www.gnu.org/licenses/gpl.html GNU General Public License
+ *
+ * Copyright (C) 2013  Peter Elmered
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @package wp-pjax
  */
-/*
-  Copyright 2013 Peter Elmered (email: peter@elmered.com)
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License, version 2, as
-  published by the Free Software Foundation.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -39,7 +42,7 @@ if ( is_admin() ) {
 /**
  * Returns instance of singleton class
  *
- * @param string $class
+ * @param string $class Short class name.
  *
  * @return object
  */
@@ -52,12 +55,17 @@ function wp_pjax_get_instance( $class ) {
 		$instances[ $class ] = new $classname();
 	}
 
-	return $instances[ $class ]; // Don't return reference
+	return $instances[ $class ]; // Don't return reference!
 }
 
 global $wp_pjax_options;
 
 if ( ! function_exists( 'is_pjax_request' ) ) {
+	/**
+	 * Check is request from pjax
+	 *
+	 * @return bool
+	 */
 	function is_pjax_request() {
 		if ( defined( 'IS_PJAX' ) && IS_PJAX ) {
 			return true;
@@ -79,6 +87,13 @@ if ( ! function_exists( 'is_pjax_request' ) ) {
 }
 
 if ( ! function_exists( 'wp_pjax_check_request' ) ) {
+	/**
+	 * Checks header as flag value
+	 *
+	 * @param string $check Header name.
+	 *
+	 * @return bool
+	 */
 	function wp_pjax_check_request( $check ) {
 		$define_key = 'IS_' . $check;
 
@@ -103,40 +118,55 @@ if ( ! function_exists( 'wp_pjax_check_request' ) ) {
 }
 
 if ( ! function_exists( 'get_pjax_header' ) ) {
+	/**
+	 * Should wp_header execution be skipped
+	 *
+	 * @return bool
+	 */
 	function get_pjax_header() {
 		if ( is_pjax_request() ) {
-			//Return TRUE to skip execution of wp_header
+			// Return TRUE to skip execution of wp_header.
 			return true;
 		} else {
-			//Return FALSE to execute wp_header
+			// Return FALSE to execute wp_header.
 			return false;
 		}
 	}
 }
 
 if ( ! function_exists( 'get_pjax_footer' ) ) {
+	/**
+	 * Should wp_footer execution be skipped
+	 *
+	 * @return bool
+	 */
 	function get_pjax_footer() {
 		if ( is_pjax_request() ) {
 			do_action( 'get_pjax_footer' );
 
-			//Return TRUE to skip execution of wp_footer
+			// Return TRUE to skip execution of wp_footer.
 			return true;
 		} else {
-			//Return FALSE to execute wp_footer
+			// Return FALSE to execute wp_footer.
 			return false;
 		}
 	}
 }
 
 if ( ! function_exists( 'get_pjax_sidebar' ) ) {
+	/**
+	 * Should sidebar execution be skipped
+	 *
+	 * @return bool
+	 */
 	function get_pjax_sidebar() {
 		if ( is_pjax_request() ) {
 			do_action( 'wp_pjax_sidebar' );
 
-			//Return TRUE to skip execution of sidebar
+			// Return TRUE to skip execution of sidebar.
 			return true;
 		} else {
-			//Return FALSE to execute sidebar
+			// Return FALSE to execute sidebar.
 			return false;
 		}
 	}
@@ -146,7 +176,7 @@ if ( ! function_exists( 'wp_pjax_header' ) ) {
 	add_action( 'wp_pjax_header', 'wp_pjax_header', 10, 3 );
 
 	/**
-	 * Prints the <title> tag based on what is being viewed.
+	 * Prints the <title> tag based on what is being viewed
 	 *
 	 * @param WP                $wp         Current WordPress environment instance.
 	 * @param WP_PJAX_PageCache $page_cache Instance of PageCache.
@@ -165,6 +195,11 @@ if ( ! function_exists( 'wp_pjax_header' ) ) {
 }
 
 add_filter( 'wp_pjax_title', 'wp_pjax_title' );
+/**
+ * Adds blog name and description to wp_title
+ *
+ * @return string
+ */
 function wp_pjax_title() {
 	$title = wp_title( '|', false, 'right' );
 
